41ad6dacac0c605a08208319af3fae32c4755c30
“Hsu <hsuyu27@gmail.com>
2021-09-04 06:17:03 +0800

    first commit

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,38 @@
-$(function() {
+$(function () {
   console.log('Hello Bootstrap5');
+});
+
+//Swiper
+var courseSwiper = new Swiper(".courseSwiper", {
+  slidesPerColumnFill: 'row',
+  slidesPerView: 2,
+  slidesPerColumn: 2,
+  spaceBetween: 30,
+  loop: true,
+  breakpoints: {
+    992: {
+      slidesPerView: 1.5,
+      slidesPerColumn: 1,
+    },
+  },
+});
+
+
+// 
+
+var mySwiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  slidesPerColumn: 3,
+  slidesPerColumnFill: 'row',
+  spaceBetween: 30,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      slidesPerColumn: 2,
+    },
+    992: {
+      slidesPerView: 3,
+      slidesPerColumn: 1,
+    },
+  },
 });
