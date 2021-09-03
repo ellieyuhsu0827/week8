34f96e72a64537a3f8d89f609d6834ff5e6ba7cf
“Hsu <hsuyu27@gmail.com>
2021-09-04 06:16:50 +0800

    Update 2021-09-03T22:16:49.688Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -2,5 +2,36 @@
 
 $(function () {
   console.log('Hello Bootstrap5');
+}); //Swiper
+
+var courseSwiper = new Swiper(".courseSwiper", {
+  slidesPerColumnFill: 'row',
+  slidesPerView: 2,
+  slidesPerColumn: 2,
+  spaceBetween: 30,
+  loop: true,
+  breakpoints: {
+    992: {
+      slidesPerView: 1.5,
+      slidesPerColumn: 1
+    }
+  }
+}); // 
+
+var mySwiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  slidesPerColumn: 3,
+  slidesPerColumnFill: 'row',
+  spaceBetween: 30,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      slidesPerColumn: 2
+    },
+    992: {
+      slidesPerView: 3,
+      slidesPerColumn: 1
+    }
+  }
 });
 //# sourceMappingURL=all.js.map
